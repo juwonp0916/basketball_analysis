09725065efe87deb7e08704ae98d8363a63f21cd
clydelhj <clydelhj@gmail.com>
2026-02-07 18:07:57 +0800

    Changed test video

--- frontend/src/routes/live/v2/+page.svelte
+++ frontend/src/routes/live/v2/+page.svelte
@@ -119,7 +119,7 @@
     simulationMode = true;
 
     videoElement.crossOrigin = "anonymous";
-    videoElement.src = `${BACKEND_URL}/video/video1.mp4`;
+    videoElement.src = `${BACKEND_URL}/video/video5.mp4`;
 
     await new Promise<void>((resolve) => {
       videoElement!.addEventListener("canplay", () => resolve(), { once: true });
